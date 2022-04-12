c5e51a0b5f0ae0a52b9afed866ba76ffacaac554
Evan <evanmarkert@gmail.com>
2022-04-12 17:09:46 -0400

    initial operators and layout of app

---
+++ app.js
@@ -0,0 +1,27 @@
+function add(a, b) {return a + b}
+function subtract(a, b) {return a - b}
+function multiply(a, b) {return a * b}
+function divide(a, b) {return a / b}
+
+function operate(operator, a, b) {
+    a = Number(a)
+    b = Number(b)
+    switch (operator) {
+        case '+':
+            return add(a, b)
+            break;
+        case '-':
+            return subtract(a, b)
+            break;
+        case '*':
+            return multiply(a, b)
+            break;
+        case '÷':
+            return divide(a, b)
+            break;
+        default:
+            return null
+            break;
+    }
+}
+
